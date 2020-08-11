04f5fac5273940901284a0548281592360b10a0e
Brendan Norris <brendan.t.norris@gmail.com>
2020-08-11 10:43:31 -0700

    working somewhat

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public float jumpSpeed = 8f;
+    public float gravity = 9.8f;
+    float vSpeed = 0; // current vertical velocity
+
+    private void Start()
+    {
+        Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+        Vector3 vel = transform.forward * Input.GetAxis("Vertical") * speed;
+        
+        if (controller.isGrounded)
+        {
+            vSpeed = 0; // grounded character has vSpeed = 0...
+            if (Input.GetKeyDown("space"))
+            { // unless it jumps:
+                vSpeed = jumpSpeed;
+            }
+        }
+        // apply gravity acceleration to vertical speed:
+        vSpeed -= gravity * Time.deltaTime;
+        vel.y = vSpeed; // include vertical speed in vel
+                        // convert vel to displacement and Move the character:
+        controller.Move(vel * Time.deltaTime);
+    }
+}
